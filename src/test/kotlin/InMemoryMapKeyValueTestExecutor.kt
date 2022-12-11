3beb906d9027735c52360eaae7ed9014edf3053e
Nikita Kraev <kitttn9@gmail.com>
2022-12-10 18:14:44 -0800

    [store] Completing test runner, now it shows the test status correctly

--- src/test/kotlin/InMemoryMapKeyValueTestExecutor.kt
+++ src/test/kotlin/InMemoryMapKeyValueTestExecutor.kt
@@ -1,9 +1,14 @@
+import command.CommandExecutor
+import impl.InMemoryMapKeyValueStorage
+import model.ExecutionResult
+import model.TestParseResult
 import java.io.File
 import java.io.FileInputStream
 import kotlin.test.Test
 
 class InMemoryMapKeyValueTestExecutor {
     private val testParser = TestParser()
+    private val storage = InMemoryMapKeyValueStorage()
 
     @Test
     fun readAndExecuteTests() {
@@ -17,9 +22,33 @@ class InMemoryMapKeyValueTestExecutor {
     }
 
     private fun executeTest(test: File) {
+        println(">> Running test: ${test.name}...")
         val inputStream = FileInputStream(test)
         val lines = inputStream.bufferedReader().readLines()
         val parsed = testParser.parseTest(lines)
-        println(">> Test: $parsed")
+        try {
+            parsed.forEach { it.assertExpected() }
+        } catch (e: AssertionError) {
+            println("❌ Test failed")
+            return
+        }
+
+        println("✅ Test completed successfully!")
+    }
+
+    private fun TestParseResult.assertExpected() = when (this) {
+        is TestParseResult.CommandOnly -> {
+            val execution = CommandExecutor.execute(command, storage)
+            assert(execution is ExecutionResult.Success)
+        }
+
+        is TestParseResult.CommandAndResult -> {
+            val expectedResultMessage =
+                when (val executionResult = CommandExecutor.execute(command, storage)) {
+                    is ExecutionResult.Success -> executionResult.messageToPrint ?: ""
+                    is ExecutionResult.Error -> executionResult.errorMessage
+                }
+            assert(expectedResultMessage == result.expected)
+        }
     }
 }
